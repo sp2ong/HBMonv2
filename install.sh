5fdbe8dff2f936d8013ba22a6f5baeaba6d2dbce
Waldek <sp2ong@gmail.com>
2020-12-31 15:20:42 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Install the required support programs
+apt-get install python3 python3-pip python3-dev libffi-dev libssl-dev -y
+pip3 install setuptools wheel
+pip3 install -r requirements.txt
