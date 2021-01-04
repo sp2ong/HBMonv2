449c6427b715952098c34bce64a79bdbfd212a0c
Waldek <sp2ong@gmail.com>
2021-01-04 19:23:11 +0100

    add sed required to install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # Install the required support programs
-apt-get install python3 python3-pip python3-dev libffi-dev libssl-dev -y
+apt-get install python3 python3-pip python3-dev libffi-dev libssl-dev sed -y
 pip3 install setuptools wheel
 pip3 install -r requirements.txt
