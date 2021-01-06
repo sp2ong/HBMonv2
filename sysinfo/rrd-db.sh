05052db552f1a3ff1dbaa9fbf4605a64b0458d4a
Waldek <sp2ong@gmail.com>
2021-01-06 13:25:44 +0100

    Add files via upload

---
+++ sysinfo/rrd-db.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# Db for temperature CPU
+rrdtool create tempC.rrd \
+--step 300 \
+DS:temp:GAUGE:600:0:100 \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:3:672 \
+RRA:MIN:0.5:1:288 \
+RRA:MIN:0.5:3:672 \
+RRA:MAX:0.5:1:288 \
+RRA:MAX:0.5:3:672 \
+RRA:LAST:0.5:1:288 \
+RRA:LAST:0.5:3:672 
+
+# Db for memory usage
+rrdtool create mem.rrd \
+--step 300 \
+DS:mem:GAUGE:600:0:100 \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:3:672 \
+RRA:MIN:0.5:1:288 \
+RRA:MIN:0.5:3:672 \
+RRA:MAX:0.5:1:288 \
+RRA:MAX:0.5:3:672 \
+RRA:LAST:0.5:1:288 \
+RRA:LAST:0.5:3:672 
+
+# Db for disk usage
+rrdtool create hdd.rrd \
+--step 300 \
+DS:hdd:GAUGE:600:0:100 \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:3:672 \
+RRA:MIN:0.5:1:288 \
+RRA:MIN:0.5:3:672 \
+RRA:MAX:0.5:1:288 \
+RRA:MAX:0.5:3:672 \
+RRA:LAST:0.5:1:288 \
+RRA:LAST:0.5:3:672 
+
+# Db for CPU load
+rrdtool create load.rrd -s 60 \
+DS:load1:GAUGE:180:0:U \
+DS:load5:GAUGE:180:0:U \
+DS:load15:GAUGE:180:0:U \
+DS:cpuuser:COUNTER:180:0:100 \
+DS:cpunice:COUNTER:180:0:100 \
+DS:cpusystem:COUNTER:180:0:100 \
+RRA:AVERAGE:0.5:1:1440 \
+RRA:AVERAGE:0.5:1440:1 \
+RRA:MIN:0.5:1440:1 \
+RRA:MAX:0.5:1440:1
