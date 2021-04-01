b47f213fe67df947561ba1ab6401b8477e00badf
Waldek <sp2ong@gmail.com>
2021-04-01 15:30:19 +0200

    Update config_SAMPLE.py

--- config_SAMPLE.py
+++ config_SAMPLE.py
@@ -49,11 +49,11 @@ WEB_PASS =  'hblink'
 
 # Files and stuff for loading alias files for mapping numbers to names
 PATH            = './'                           # MUST END IN '/'
-PEER_FILE       = 'peer_ids.json'                # Will auto-download from DMR-MARC
-SUBSCRIBER_FILE = 'subscriber_ids.json'          # Will auto-download from DMR-MARC
-TGID_FILE       = 'talkgroup_ids.json'           # User provided, should be in "integer TGID, TGID name" format
-LOCAL_SUB_FILE  = 'local_subscriber_ids.json'    # User provided (optional, leave '' if you don't use it), follow the format of DMR-MARC
-LOCAL_PEER_FILE = 'local_peer_ids.json'          # User provided (optional, leave '' if you don't use it), follow the format of DMR-MARC
+PEER_FILE       = 'peer_ids.json'                # Will auto-download 
+SUBSCRIBER_FILE = 'subscriber_ids.json'          # Will auto-download 
+TGID_FILE       = 'talkgroup_ids.json'           # User provided
+LOCAL_SUB_FILE  = 'local_subscriber_ids.json'    # User provided (optional, leave '' if you don't use it)
+LOCAL_PEER_FILE = 'local_peer_ids.json'          # User provided (optional, leave '' if you don't use it)
 FILE_RELOAD     = 30                              # Number of days before we reload DMR-MARC database files
 PEER_URL        = 'https://database.radioid.net/static/rptrs.json'
 SUBSCRIBER_URL  = 'https://database.radioid.net/static/users.json'
