0630602e3c64c6281bcae4d9943683add48d5126
Waldek <sp2ong@gmail.com>
2021-05-18 17:29:41 +0200

    Add files via upload

---
+++ html/include/config.php
@@ -0,0 +1,39 @@
+<?php
+
+// Report all errors except E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE);
+
+
+// Name of the monitored Dashboard
+
+define("REPORT_NAME","Dashboard of local DMR network");
+
+//
+// Theme colors define
+//
+// Green 
+//define("THEME_COLOR","background-color:#4a8f3c;color:white;");
+
+// Blue 1
+//define("THEME_COLOR","background-color:#2A659A;color:white;");
+
+// Blue 2
+//define("THEME_COLOR","background-color:#43A6DF;color:white;");
+
+// Blue Gradient 1
+define("THEME_COLOR","background-image: linear-gradient(to bottom, #337ab7 0%, #265a88 100%);color:white;");
+
+// Blue Gradient 2
+//define("THEME_COLOR","background-image: linear-gradient(to bottom, #3333cc 0%, #265a88 100%);color:white;");
+
+// Red Gradient
+//define("THEME_COLOR","background-image:linear-gradient(0deg, rgba(251,0,0,1) 0%, rgba(255,131,131,1) 50%, rgba(255,255,255,1) 100%);color:black;");
+
+// Grey Gradient 
+//define("THEME_COLOR","background-image: linear-gradient(to bottom, #3b3b3b 10%, #808080 100%);color:white;");
+
+// Green Gradient 
+//define("THEME_COLOR","background-image:linear-gradient(to bottom right,#d0e98d, #4e6b00);color:black;");
+//
+
+?>
