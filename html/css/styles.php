53b26925f9fe4bb8c1309eb88a31c3b1a954328a
Waldek <sp2ong@gmail.com>
2021-05-24 17:50:32 +0200

    Update styles.php

--- html/css/styles.php
+++ html/css/styles.php
@@ -19,7 +19,7 @@ html {
 
 table.log {background-color: #f0f0f0; border-collapse: collapse; border: 1px solid #C1DAD7; width: 100%;}
 th.log {height: 30px; text-align: center;}
-tr:nth-child(even).log {background-color: #fafafa; height: 20px; text-align: center;}
+tr:nth-child(even).log {background-color: #fafafa;text-align: center;}
 td.log {font-family: Monospace; height: 20px;}
 
 
