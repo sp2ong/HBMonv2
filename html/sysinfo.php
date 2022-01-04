d46dfc31cc83af38bbbccd134d8afe9db5d8ad3c
Waldek <sp2ong@gmail.com>
2022-01-04 07:01:51 +0100

    Update sysinfo.php

--- html/sysinfo.php
+++ html/sysinfo.php
@@ -11,7 +11,7 @@ include_once 'include/config.php';
 <title>DMR Server monitor - System Info</title>
 <script type="text/javascript" src="scripts/hbmon.js"></script>
 <link rel="stylesheet" type="text/css" href="css/styles.php" />
-<meta name="description" content="Copyright (c) 2016-2021.The Regents of the K0USY Group. All rights reserved. Version SP2ONG 2019-2021 (v2021)" />
+<meta name="description" content="Copyright (c) 2016-2022.The Regents of the K0USY Group. All rights reserved. Version SP2ONG 2019-2022" />
 </head>
 <body style="background-color: #d0d0d0;font: 10pt arial, sans-serif;">
 <center><div style="width:1250px; text-align: center; margin-top:5px;">
@@ -46,9 +46,9 @@ include_once 'include/config.php';
 </fieldset>
 <br>
 <p style="text-align: center;"><span style="text-align: center;">
-Copyright (c) 2016-2021<br>The Regents of the <a href=http://k0usy.mystrikingly.com/>K0USY Group</a>. All rights reserved.<br><a href=https://github.com/sp2ong/HBMonv2>Version SP2ONG 2019-2021</a><br><br></span>
+Copyright (c) 2016-2021<br>The Regents of the <a href=http://k0usy.mystrikingly.com/>K0USY Group</a>. All rights reserved.<br><a href=https://github.com/sp2ong/HBMonv2>Version SP2ONG 2019-2022</a><br><br></span>
     <!-- THIS COPYRIGHT NOTICE MUST BE DISPLAYED AS A CONDITION OF THE LICENCE GRANT FOR THIS SOFTWARE. ALL DERIVATEIVES WORKS MUST CARRY THIS NOTICE -->
-    <!-- This is version of HBMonitor SP2ONG 2019-2021 (v2021) -->
+    <!-- This is version of HBMonitor SP2ONG 2019-2022 -->
 </p>
 </center>
 </body>
