a73663f1c11b55c5044323f0efd08c29d9291b18
Waldek <sp2ong@gmail.com>
2022-01-04 07:06:15 +0100

    Update moni.php

--- html/moni.php
+++ html/moni.php
@@ -33,7 +33,7 @@ include_once 'include/config.php';
 <pre id="log" style="height: 40em; text-align: left; overflow-y: scroll; font-size:13px; background-color: #000000; color:#b1eee9;"></pre>
 </fieldset></div>
 <p style="text-align: center;"><span style="text-align: center;">
-Copyright (c) 2016-2021<br>The Regents of the <a href=http://k0usy.mystrikingly.com/>K0USY Group</a>. All rights reserved.<br><a href=https://github.com/sp2ong/HBMonv2>Version SP2ONG 2019-2022</a><br><br></span>
+Copyright (c) 2016-2022<br>The Regents of the <a href=http://k0usy.mystrikingly.com/>K0USY Group</a>. All rights reserved.<br><a href=https://github.com/sp2ong/HBMonv2>Version SP2ONG 2019-2022</a><br><br></span>
     <!-- THIS COPYRIGHT NOTICE MUST BE DISPLAYED AS A CONDITION OF THE LICENCE GRANT FOR THIS SOFTWARE. ALL DERIVATEIVES WORKS MUST CARRY THIS NOTICE -->
     <!-- This is version of HBMonitor SP2ONG 2019-2022 -->
 </p>
