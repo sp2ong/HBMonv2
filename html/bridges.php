bd8a80a9380500cad31fa671f17a2faafee181e5
Waldek <sp2ong@gmail.com>
2022-03-13 16:46:32 +0100

    changes posted by M0VUB

--- html/bridges.php
+++ html/bridges.php
@@ -10,10 +10,10 @@ include_once 'include/config.php';
 <title>DMR Server monitor - STATUS</title>
 <script type="text/javascript" src="scripts/hbmon.js"></script>
 <link rel="stylesheet" type="text/css" href="css/styles.php" />
-<meta name="description" content="Copyright (c) 2016-2022.The Regents of the K0USY Group. All rights reserved. Version SP2ONG 2019-2022" />
+<meta name="description" content="Copyright &copy; 2016-2022.The Regents of the K0USY Group. All rights reserved. Version SP2ONG 2019-2022" />
 </head>
 <body style="background-color: #d0d0d0;font: 10pt arial, sans-serif;">
-<center><div style="width:1250px; text-align: center; margin-top:5px;">
+<center><div style="width:1100px; text-align: center; margin-top:5px;">
 <img src="img/logo.png?random=323527528432525.24234" alt="" />
 </div>
 <div style="width: 1100px;">
@@ -26,7 +26,7 @@ include_once 'include/config.php';
 	</div>
 <div style="width: 1100px; margin-left:0px;">
 <p style="text-align: center;"><span style="text-align: center;">
-    Copyright (c) 2016-2022<br>The Regents of the <a href=http://k0usy.mystrikingly.com/>K0USY Group</a>. All rights reserved.<br><a href=https://github.com/sp2ong/HBMonv2>Version SP2ONG 2019-2022</a><br><br></span>
+    Copyright &copy; 2016-2022<br>The Regents of the <a href=http://k0usy.mystrikingly.com/>K0USY Group</a>. All rights reserved.<br><a href=https://github.com/sp2ong/HBMonv2>Version SP2ONG 2019-2022</a><br><br></span>
     <!-- THIS COPYRIGHT NOTICE MUST BE DISPLAYED AS A CONDITION OF THE LICENCE GRANT FOR THIS SOFTWARE. ALL DERIVATEIVES WORKS MUST CARRY THIS NOTICE -->
     <!-- This is version of HBMonitor SP2ONG 2019-2022 -->
 </p></div>
